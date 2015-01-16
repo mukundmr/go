df085bde0ed1634bdc476a8b5a2786b7cd596670
Mukund Rajamannar <mukund@synerzip.com>
2015-01-16 16:10:10 +0530

    broken prime

--- prime/prime.go
+++ prime/prime.go
@@ -3,95 +3,139 @@ package main
 import (
 	"fmt"
 	"math/big"
-	"sync"
-	"time"
+	//	"sync"
+	//	"time"
 )
 
-/* prime numbers */
+type splitMap map[string]*big.Int
 
-func isPrime(num_to_check *big.Int) (ret bool) {
-	// pathetic algorithm.  not meant for anything other than messing with language constructs
-	var tmp1, zero, two, three, four, eight big.Int
-	var modres *big.Int = big.NewInt(int64(0))
+/* some handy constants. big.Int isn't friendly */
+var zero = big.NewInt(int64(0))
+var one = big.NewInt(int64(1))
+var two = big.NewInt(int64(2))
+var three = big.NewInt(int64(3))
+var four = big.NewInt(int64(4))
+var eight = big.NewInt(int64(8))
 
-	tmp1.Set(num_to_check)
-	ret = true
-	four.SetInt64(int64(4))
-	three.SetInt64(int64(3))
-	two.SetInt64(int64(2))
-	zero.SetInt64(int64(0))
-	eight.SetInt64(int64(8))
-
-	if tmp1.Cmp(&four) < 0 && tmp1.Cmp(&zero) > 0 {
-		return
-	}
-
-	modres = modres.Mod(&tmp1, &two)
-
-	if modres.Cmp(&zero) == 0 {
-		fmt.Println("it is a even number ")
-		ret = false
-		return
-	}
-	// split the range of 3..num/2 into 4 (1/8, 2/8, 3/8, 4/8)
-	var splits map[string]*big.Int = make(map[string]*big.Int)
+/* create splits to use */
+func get_splits(num *big.Int) (splits splitMap) {
 	var one_eight, two_eight, three_eight, four_eight big.Int
-	one_eight.Div(&tmp1, &eight)
-	two_eight.Div(&tmp1, &eight)
-	two_eight.Mul(&two_eight, &two)
-	three_eight.Div(&tmp1, &eight)
-	three_eight.Mul(&three_eight, &three)
-	four_eight.Div(&tmp1, &eight)
-	four_eight.Mul(&four_eight, &four)
+	make_odd(one_eight.Div(num, eight))
+	make_odd(two_eight.Mul(&one_eight, two))
+	make_odd(three_eight.Mul(&one_eight, three))
+	make_odd(four_eight.Mul(&one_eight, four))
+	splits = make(splitMap)
 	splits[three.String()] = &one_eight // 3..x/8
 	splits[one_eight.String()] = &two_eight
 	splits[two_eight.String()] = &three_eight
 	splits[three_eight.String()] = &four_eight
+	return
+}
 
-	resChan := make(chan bool, 5)
-	resChan <- true
-	divByChan := make(chan string, 1)
-	var wg sync.WaitGroup
-	wg.Add(len(splits))
-	fmt.Println(len(splits), " tracks.")
-	for key, val := range splits {
-		go func(start string, end *big.Int) {
-			var local_itr big.Int
-			local_itr.SetString(start, 10)
-			var local_tmp big.Int
-			defer wg.Done()
-			fmt.Println("Running from ", local_itr.String(), " to ", (*end).String())
-			for ; local_itr.Cmp(end) < 0; local_itr.Add(&local_itr, &two) {
-				if local_tmp.Mod(&tmp1, &local_itr).Cmp(&zero) == 0 {
-					divByChan <- local_itr.String()
-					resChan <- false
-					wg.Done()
-					wg.Done()
-					wg.Done()
+/* helpers */
+func isDivisible(num_to_check *big.Int, divisor *big.Int) (isDivisible bool) {
+	var tmp big.Int
+	isDivisible = (tmp.Rem(num_to_check, divisor).Cmp(zero) == 0)
+	return
+}
+
+/* make odd */
+func make_odd(num_to_check *big.Int) {
+	if isDivisible(num_to_check, two) {
+		num_to_check.Sub(num_to_check, one)
+	}
+}
+
+/* evaluation loop */
+func looper(num_to_check *big.Int, start, end *big.Int, quit chan bool) (ret chan *big.Int) {
+	ret = make(chan *big.Int)
+	fmt.Println(start, end)
+	go func() {
+		var interrupt = false
+		var itr big.Int
+		for itr.Set(start); itr.Cmp(end) < 0; itr.Add(&itr, two) {
+			select {
+			default:
+				if isDivisible(num_to_check, &itr) {
+					interrupt = true
+					quit <- true
+					var val big.Int
+					val.Set(&itr)
+					ret <- &val
+					fmt.Println("returning ", itr.String(), " from ", start, " to ", end)
 					break
 				}
+			case <-quit:
+				interrupt = true
+				ret <- zero
+				quit <- true
+				fmt.Println("interupt due to quit from ", start, " to ", end)
+				break
 			}
-			fmt.Println("Running from ", start, " to ", (*end).String(), " done.")
-		}(key, val)
-	}
-	wg.Wait() // fake wait. if one routine finishes, the wg is freed. we don't care about other routines
-	for _ = range resChan {
-		select {
-		case ret = <-resChan:
-			close(resChan) // assigned true first time. rest depends on Go routine
-		case <-time.After(time.Millisecond): // one millisecond timeout.  wonder if one microsecond makes sense
-			continue
 		}
+		if !interrupt {
+			ret <- zero
+			fmt.Println("returning ", zero.String())
+		}
+	}()
+	return
+}
+
+/* mux */
+func mux4to1(a, b, c, d <-chan *big.Int) (ret chan *big.Int) {
+	ret = make(chan *big.Int)
+	go func() {
+		for {
+			select {
+			case z := <-a:
+				ret <- z
+			case z := <-b:
+				ret <- z
+			case z := <-c:
+				ret <- z
+			case z := <-d:
+				ret <- z
+			}
+		}
+	}()
+	return
+}
+
+/* prime numbers */
+
+func isPrime(num_to_check *big.Int) (isPrime bool) {
+	isPrime = true
+	if num_to_check.Cmp(four) < 0 {
+		return
+	} else if isDivisible(num_to_check, two) {
+		isPrime = false
+		return
 	}
-	if ret == false {
-		fmt.Println("Divisible by ", <-divByChan)
+	splits := get_splits(num_to_check)
+	var stream = make(chan (chan *big.Int), len(splits))
+	var quit = make(chan bool)
+	var start,stop big.Int
+	for key, value := range splits {
+		start.SetString(key, 10)
+		stop.Set(value)
+		stream <- looper(num_to_check, &start, &stop, quit)
+	}
+	resultChan := mux4to1(<-stream, <-stream, <-stream, <-stream)
+	for waitloop := 0; waitloop < len(splits); waitloop++ {
+		select {
+		case val := <-resultChan:
+			if val.Cmp(zero) != 0 {
+				fmt.Println(num_to_check, " divisible by ", val)
+				isPrime = false
+			}
+		}
 	}
 	return
 }
 
 func main() {
 	var i big.Int
-	i.SetString("1347852092143232009187340981273401274309812734901234079109283881", 10)
+	i.SetString("9513", 10)
 	if isPrime(&i) {
 		fmt.Println(i.String(), "is a prime number")
 	} else {
